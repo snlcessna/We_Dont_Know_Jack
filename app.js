feb8b3349cd1aae883c47eb1658c7984b3326b48
jrzollin <jrzollin@yahoo.com>
2017-07-08 10:18:46 -0700

    constructor working, 5 questions added

--- app.js
+++ app.js
@@ -1,19 +1,32 @@
 'use strict';
 
 //functions=====================================================================
-function Question(question, correct, incorrect1, incorrect2, incorrect3, cat) {
+function Question(question, correct, incorrect1, incorrect2, incorrect3, cat){
   this.question = question;
   this.correct = correct;
   this.incorrect1 = incorrect1;
   this.incorrect2 = incorrect2;
   this.incorrect3 = incorrect3;
   this.cat = cat;
-  cat.push(this);
+  this.catPush();
 }
 
+Question.prototype.catPush = function(){
+  if(this.cat = 'codeCat'){
+    codeCat.push(this);
+  }
+}
 //arrays========================================================================
 var codeCat = [];
 
 //variables=====================================================================
 //questions
-var codeQeustion1 = new Question('What are the CSS values for the display property', 'Inline, Block, Inline-block, None', 'Inline, Block, Hidden, Visible', 'Visible, Invisible, Inline, Block', 'Block, Inline, Inline-block, Hidden', 'codeCat');
+var codeQeustion1 = new Question('What are the CSS values for the display property?', 'Inline, Block, Inline-block, None', 'Inline, Block, Hidden, Visible', 'Visible, Invisible, Inline, Block', 'Block, Inline, Inline-block, Hidden', 'codeCat');
+
+var codeQuestion2 = new Question('What bullet point styles can you use for unordered lists?', 'None, Disc, Circle, Square', 'Circle, Star, Dot, Square', 'Decimal, Dot, None, Star', 'Oval, Circle, Square, Star', 'codeCat');
+
+var codeQuestion3 = new Question('When using an image for a background, how do you get the image to repeat vertially?', 'repeat-y', 'repeat', 'repeat-x', 'scroll', 'codeCat');
+
+var codeQuestion4 = new Question('What index is the \"4\" in within the following array?  numbers = [7, 1, 8, monkey, 12, 3, 4]', 6, 3, 8, 7, 'codeCat');
+
+var codeQuestion5 = new Question('How do you round a number to the nearest integer?', 'Math.round()', 'Math.floor()', 'Math.integer()', 'Math.ceil()', 'codeCat');
